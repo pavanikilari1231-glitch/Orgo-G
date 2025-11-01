fe07ee5dd2ba38c575bfd72d21ddcbb755d456c2
Pavani Kilari <pavanikilari1231@gmail.com>
2025-11-01 12:08:15 +0530

    initial commits

---
+++ queries.js
@@ -0,0 +1,11 @@
+function validateLogin() {
+    var image = document.getElementById('image').value;
+    var symptoms = document.getElementById('symptoms').value;
+
+    // Example validation (replace this with your authentication logic)
+    if (image === 'image' && "symptoms" === 'symptoms') {
+        <a href="f1.html">ENTER</a>;
+    } else {
+        <a href="f2.html">ENTER</a>;
+    }
+}
